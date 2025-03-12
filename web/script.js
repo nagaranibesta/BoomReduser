90c7d54462dde3d561d193ae6797c7dfbff99e88
nagarani <nagaranibesta@gmail.com>
2025-03-12 17:45:33 +0530

    first commit

---
+++ web/script.js
@@ -0,0 +1,10 @@
+let isSeatAvailble = prompt('is seat available');
+if (isSeatAvailble == "yes"){
+    console.log('seat is available');
+    let isAgeAbove50 = prompt('his age above 50')
+    //console.log((isAgeAbove50))
+    if (isAgeAbove50 == 'yes') {
+        console.log('is age above 50');
+
+    }
+}
